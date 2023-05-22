f31396b4c50680f81411ec8b8a68ee0d43395bc6
Christian Chambers <christian@stokeham.net>
2023-05-22 21:25:09 +0100

    Hack FT runner to be able to test staging

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,13 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, re_path, include
 from lists import views as list_views
 from lists import urls as list_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', list_views.home_page, name='home'),
     path('lists/', include(list_urls)),
 ]
