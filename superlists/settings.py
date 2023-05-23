df35050d3d698b9b28d840a95a6263cb4be295dd
Christian Chambers <christian@stokeham.net>
2023-05-23 20:30:12 +0100

    CSRF Origin fix

--- superlists/settings.py
+++ superlists/settings.py
@@ -123,3 +123,7 @@ STATIC_ROOT = BASE_DIR /'static'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CSRF_TRUSTED_ORIGINS A list of trusted origins for unsafe requests
+# https://docs.djangoproject.com/en/4.2/ref/csrf/
+CSRF_TRUSTED_ORIGINS = ['http://*.stokeham.net']
